0cee309101386a5c96f0c04889cbc51838dbc604
KateShkuryna <shkurinakate@gmail.com>
2024-12-24 15:19:34 +0200

    Added initial version of the run function

--- src/main/java/org/example/TaskManager.java
+++ src/main/java/org/example/TaskManager.java
@@ -12,4 +12,20 @@ public class TaskManager {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
     }
+
+    private void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nTask Manager");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Task");
+            System.out.println("3. Edit Task");
+            System.out.println("4. Delete Task");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+        }
+    }
 }
